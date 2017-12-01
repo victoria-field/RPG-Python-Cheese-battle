b8bbb9856c84f5ad007b92ec63b73d70c7292745
victoria field <victoria.shadid@gmail.com>
2017-12-01 14:56:57 -0600

    Added initial code.

---
+++ classes/game.py
@@ -0,0 +1,91 @@
+import random
+# from .magic import Spell
+# from .inventory import Items
+#pretty print
+import pprint
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE =  '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+#self means this instance of the class Person
+class Person:
+    def __init__(self, name, hp, mp, atk, df, magic, items):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.items = items
+        self.actions = ["Attack", "Magic", "Items"]
+        self.name = name
+
+#this returns a random number between the atk low and atk high
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    def heal(self, dmg):
+        self.hp += dmg
+        if self.hp > self.maxhp:
+            self.hp = self.maxhp
+
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def choose_action(self):
+        i = 1
+        print("\n" + bcolors.OKBLUE + bcolors.BOLD + "ATTACK" + bcolors.ENDC)
+        for item in self.actions:
+            print("    " + str(i) + ".", item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+
+        print("\n" + bcolors.OKBLUE + bcolors.BOLD + "MAGIC" + bcolors.ENDC)
+        for spell in self.magic:
+            #spell.name and spell.cost is allowing us to choose the item as a object
+            print("    " + str(i) + ".", spell.name, "(cost:", str(spell.cost) + ")")
+            i += 1
+
+    def choose_item(self):
+        i = 1
+
+        print("\n" + bcolors.OKGREEN + bcolors.BOLD + "ITEMS:" + bcolors.ENDC)
+        for item in self.items:
+            print("    " + str(i) + ".", item["item"].name + ":", item["item"].description, " (x" + str(item["quantity"]) +")")
+            i += 1
+
+    def get_stats(self):
+        print("                            _________________________                    __________")
+        print(bcolors.BOLD + self.name +"        " +
+          str(self.hp) + "/" + str(self.maxhp) + "  |" + bcolors.OKGREEN + "████████████            " + bcolors.ENDC + bcolors.BOLD + "|            "+
+          str(self.mp) + "/" + str(self.maxmp) + " |" + bcolors.OKBLUE + "██████████" + bcolors.ENDC + "|")
